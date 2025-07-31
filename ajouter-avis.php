8f337b8a6e5fd211ccb9043d79c21bb0e061826e
yakubonal <yakubonal6601@gmail.com>
2025-07-31 15:24:32 +0200

    rajoutHoneypot et details avis

--- ajouter-avis.php
+++ ajouter-avis.php
@@ -1,6 +1,13 @@
 <?php
 require 'db.php';
 
+// Anti-spam honeypot
+if (!empty($_POST['website'])) {
+    // Tentative de spam : on ne fait rien
+    http_response_code(400);
+    exit;
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nom = htmlspecialchars($_POST['nom']);
     $message = htmlspecialchars($_POST['message']);
